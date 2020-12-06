5927353b48210c5055a5d58d421f6c61625902d8
Kaia Leahy <canhascodez@gmail.com>
2020-12-06 00:33:21 -0500

    reduce Crystal Day 06 solution slightly

--- 06/crystal.cr
+++ 06/crystal.cr
@@ -3,8 +3,7 @@ INPUT = File.read("#{__DIR__}/input.txt").split("\n\n")
 first = INPUT.sum(&.chars.uniq.-(['\n']).size)
 
 second = INPUT.sum do |record|
-  chars = record.lines.map(&.chars)
-  chars.reduce(chars.first) {|m, e| m & e }.size
+  record.lines.map(&.chars).reduce {|m, e| m & e }.size
 end
 
 p first
